2c5fb454121f62462a7fc8f5721ac3a9f7e13e9f
sarah <sarah134632@gmail.com>
2024-07-28 09:49:20 +0300

    my project

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+﻿function addT() {
+
+    for (var i = 1; i <= 40; i++) {
+        document.getElementById("colorT").innerHTML += "<tr id='tr" + i + "'></tr>";
+
+        for (var j = 1; j <= 100; j++) {
+            document.getElementById("tr" + i).innerHTML += "<td></td>";
+
+        }
+    }
+}
+function colorCol(x) {
+    var color;
+    color = x.target;
+    color.style.background = document.getElementById("cl").value;
+}
